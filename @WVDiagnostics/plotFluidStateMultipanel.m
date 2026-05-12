ac58f4d8cde8b0a589bd38f66156605d580ae2f4
Cimarron Wortham <worthamc@gmail.com>
2026-05-12 14:12:04 -0700

    Use vertical mode number for yaxis.

--- @WVDiagnostics/plotFluidStateMultipanel.m
+++ @WVDiagnostics/plotFluidStateMultipanel.m
@@ -62,16 +62,6 @@ zeta_limits = [-0.2 0.2];
 energy_limits = [-8 0];
 enstrophy_limits = [-16 -9];
 
-% % % % Create wavelength axes for wvt variables
-% % % % We will pretend the "0" wavenumber is actually evenly spaced
-% % % % from the nearest two wavenumbers
-% % % kPseudoLocation = wvt.kRadial;
-% % % kPseudoLocation(1) = exp(-log(kPseudoLocation(3)) + 2*log(kPseudoLocation(2)));
-% % % jPseudoLocation = self.jWavenumber(wvt.j+1);
-% % % jPseudoLocation(1) = exp(-log(jPseudoLocation(3)) + 2*log(jPseudoLocation(2)));
-% % % [KPseudoLocation,JPseudoLocation] = ndgrid(kPseudoLocation,jPseudoLocation);
-% % % kPseudoRadial = sqrt(JPseudoLocation.^2 + KPseudoLocation.^2);
-
 % We will pretend the "0" wavenumber is actually evenly spaced
 % from the nearest two wavenumbers
 kPseudoLocation = self.kRadial;
@@ -82,37 +72,17 @@ jwnPseudoLocation = self.jWavenumber;
 jwnPseudoLocation(1) = exp(-log(jwnPseudoLocation(3)) + 2*log(jwnPseudoLocation(2)));
 jPseudoLocation = self.j;
 jPseudoLocation(1) = exp(-log(jPseudoLocation(3)) + 2*log(jPseudoLocation(2)));
+[KPseudoLocation,JWNPseudoLocation] = ndgrid(kPseudoLocation,jwnPseudoLocation);
+kPseudoRadial = sqrt(JWNPseudoLocation.^2 + KPseudoLocation.^2);
 
-% % % % For interpolation to work correctly we need to repeat the
-% % % % first entry, but properly back at zero
-% % % % NOTE: these are from wvd, including the anti-aliased modes.
-% % % kPseudoLocationWVD = self.kRadial;
-% % % kPseudoLocationWVD(1) = exp(-log(kPseudoLocationWVD(3)) + 2*log(kPseudoLocationWVD(2)));
-% % % jPseudoLocationWVD = self.jWavenumber;
-% % % jPseudoLocationWVD(1) = exp(-log(jPseudoLocationWVD(3)) + 2*log(jPseudoLocationWVD(2)));
-% % % kPaddedWVD = cat(1,0,kPseudoLocationWVD);
-% % % jPaddedWVD = cat(1,0,jPseudoLocationWVD);
-% % % [KPaddedWVD,JPaddedWVD] = ndgrid(kPaddedWVD,jPaddedWVD);
-
-% For interpolation to work correctly we need to repeat the
-% first entry, but properly back at zero
-kPadded = cat(1,0,kPseudoLocation);
-kModePadded = cat(1,0,kModePseudoLocation);
-jwnPadded= cat(1,0,jwnPseudoLocation);
-jPadded= cat(1,0,jPseudoLocation);
-[KModePadded,JPadded] = ndgrid(kModePadded,jPadded); % mode number grids
-[KPadded,JWNPadded] = ndgrid(kPadded,jwnPadded); % wavenumber grids
-kPseudoRadial = sqrt(JWNPadded.^2 + KPadded.^2);
-
-% create some nice tick labels to show wavelength
-% ticks_x = [500;300;200;100;50;30;20;10];
-% ticks_x = [500;50;30;20;15;12;10];
-% % % ticks_x = (2*pi./(1000.*linspace(wvt.kRadial(2),wvt.kRadial(end),6)));
-% % % labels_x = cell(length(ticks_x),1);
-% % % for i=1:length(ticks_x)
-% % %     labels_x{i} = sprintf('%.0f',ticks_x(i));
-% % % end
-% % % ticks_x = 2*pi./(1e3*ticks_x);
+% % For interpolation to work correctly we need to repeat the
+% % first entry, but properly back at zero
+% kPadded = cat(1,0,kPseudoLocation);
+% kModePadded = cat(1,0,kModePseudoLocation);
+% jwnPadded= cat(1,0,jwnPseudoLocation);
+% jPadded= cat(1,0,jPseudoLocation);
+% [KModePadded,JPadded] = ndgrid(kModePadded,jPadded); % mode number grids
+% [KPadded,JWNPadded] = ndgrid(kPadded,jwnPadded); % wavenumber grids
 
 % color for some annotations
 textColor = '[.5,.5,.5]';
@@ -121,12 +91,6 @@ textColor = '[.5,.5,.5]';
 % iY = round(wvt.Nx/2);
 iY = 1;
 
-% % % % % create the lines of constant frequency
-% % % % [omegaN,n] = wvt.transformToRadialWavenumber(abs(wvt.Omega),ones(size(wvt.Omega)));
-% % % % omegaJK = (omegaN./n)/wvt.f;
-
-% % % % create the lines of constant deformation radius
-% % % deformationJK = repmat(sqrt(wvt.Lr2)./1000,1,length(wvt.kRadial));
 
 nColumns = 2;
 if options.shouldShowEnergySpectra
@@ -147,10 +111,7 @@ else
     set(0, 'currentfigure', options.figureHandle);
 end
 
-
-% % % tl = tiledlayout(2,nColumns,TileSpacing="tight");
 tl = tiledlayout(1,nColumns,TileSpacing="tight");
-
 title(tl, options.title, 'Interpreter', 'none')
 
 % compute some quantities
@@ -170,28 +131,22 @@ tl_inner = tiledlayout(tl,2,2,TileSpacing='tight');
 tl_inner.Layout.TileSpan = [1,2];
 
 % geostrophic surface vorticity
-% % % ax = nexttile(tl,1);
 ax = nexttile(tl_inner,1);
 val = zeta_z_g(:,:,end)/wvt.f;
 pcolor(ax, wvt.x/1e3, wvt.y/1e3, val.'), shading interp,
 hold on; plot(wvt.x/1e3,ones(size(wvt.x))*wvt.y(iY)/1e3,'k:');hold off; % add line for x-z section
 title("geostrophic vorticity \zeta_g")
-% title("geostrophic surface vorticity")
 axis square
 xticklabels([])
-% yticklabels([])
 ylabel('y-distance (km)')
 set(gca,'YTick',xticks,'Layer','top','TickLength',[0.015 0.015])
 colormap(ax, cmDivRWB);
 clim(ax, zeta_limits);
 
 % geostrophic vorticity section
-% % % ax = nexttile(tl,nColumns+1);
 ax = nexttile(tl_inner,3);
 val = squeeze(zeta_z_g(:,iY,:)/wvt.f);
 pcolor(ax, wvt.x/1e3, wvt.z/1e3, val.'), shading interp,
-% title("geostrophic x-z vorticity")
-% title("x-z vorticity")
 xlabel('x-distance (km)')
 ylabel('Depth (km)')
 axis square
@@ -199,44 +154,30 @@ colormap(ax, cmDivRWB);
 ylabel('Depth (km)')
 set(gca,'Layer','top','TickLength',[0.015 0.015])
 clim(ax, zeta_limits);
-% % % cb = colorbar("southoutside");
-% % % cb.Label.String = "$\zeta$/f";
-% % % cb.Label.Interpreter = 'latex';
 
 % wave surface vorticity
-% % % ax = nexttile(tl,2);
 ax = nexttile(tl_inner,2);
 val = zeta_z_w(:,:,end)/wvt.f;
 pcolor(ax, wvt.x/1e3, wvt.y/1e3, val.'), shading interp,
 hold on; plot(wvt.x/1e3,ones(size(wvt.x))*wvt.y(iY)/1e3,'k:');hold off; % add line for x-z section
-% title("surface vorticity")
 title("wave vorticity \zeta_w")
 axis square
 colormap(ax, cmDivRWB);
 yticklabels([])
 xticklabels([])
-% xlabel('x-distance (km)')
-% ylabel('y-distance (km)')
 set(gca,'YTick',xticks,'Layer','top','TickLength',[0.015 0.015])
 clim(ax, zeta_limits);
 
 % wave vorticity section
-% % % ax = nexttile(tl,nColumns+2);
 ax = nexttile(tl_inner,4);
 val = squeeze(zeta_z_w(:,iY,:)/wvt.f);
 pcolor(ax, wvt.x/1e3, wvt.z/1e3, val.'), shading interp,
-% title("wave x-z vorticity")
-% title("x-z vorticity")
 axis square
 colormap(ax, cmDivRWB);
 xlabel('x-distance (km)')
-% ylabel('Depth (km)')
 yticklabels([])
 set(gca,'Layer','top','TickLength',[0.015 0.015])
 clim(ax, zeta_limits);
-% % % cb = colorbar("southoutside");
-% % % cb.Label.String = "$\zeta$/f";
-% % % cb.Label.Interpreter = 'latex';
 
 cb = colorbar;
 cb.Layout.Tile = 'south';
@@ -250,80 +191,91 @@ if options.shouldShowEnergySpectra
     tl_inner.Layout.Tile = 3;
 
     % geostrophic energy spectrum
-    % % % ax = nexttile(tl,3);
     ax = nexttile(tl_inner,1);
     val = log10(TE_A0_j_kR);
     pcolor(ax,2*pi./kPseudoLocation/1000,jPseudoLocation,val), shading flat,
     set(gca,'XDir','reverse')
     set(gca,'XScale','log')
-    % % % set(gca,'YDir','reverse')
     set(gca,'YScale','log')
     xticklabels([])
     title('geostrophic energy spectrum')
-    % title('geostrophic energy spectrum','Units', 'normalized', 'Position', [0.5, 0.93, 0])
     axis square
     clim(ax,energy_limits);
-    % ylabel("deformation wavelength (km)")
-    % ylabel("vertical mode scale, 2\pi\cdotL_d (km)")
-    ylabel("vertical mode deformation length (km)")
+    ylabel("Vertical mode number")
     set(gca, 'YAxisLocation', 'right','Layer','top','TickLength',[0.015 0.015]);
     colormap(ax, cmLinMono);
     set(gca,'layer','top'),
     hold on
-    % add ke:pe ratio contours. 
-    % Note: have to pad quantities to work right with our log-log axes. And here
-    % have to "double pad" to work right with pcolor's box shift and
-    % reversed axis direction. 
+    % add ke:pe ratio contours. flipud/fliplr gives nicer clabel placement.
     fraction = self.geo_hke_jk./(self.geo_hke_jk+self.geo_pe_jk);
-    % fractionPadded = cat(1,fraction(1,:),fraction);
-    fractionPadded = cat(1,[fraction(1,:);fraction(1,:)],fraction(1:end-1,:));
-    % fractionPadded = cat(2,fractionPadded(:,1),fractionPadded);
-    fractionPadded = cat(2,[fractionPadded(:,1) fractionPadded(:,1)],fractionPadded(:,1:end-1));
-    fractionJK = interpn(KPadded,JPadded,fractionPadded.',kPseudoLocation,jPseudoLocation,"linear");
-    [C,h] = contour(ax,2*pi./kPseudoLocation/1000,jPseudoLocation,fractionJK,options.keFractions,'LineWidth',options.lineWidth,'Color',options.keFractionColor, DisplayName="KE/(KE+PE)", HandleVisibility='off');
+    fractionPadded = cat(1,fraction(1,:),fraction);
+    fractionPadded = cat(2,fractionPadded(:,1),fractionPadded);
+    [C,h] = contour(ax,flipud(2*pi./kPseudoLocation/1000),jPseudoLocation,fliplr(fractionPadded(1:end-1,1:end-1)),options.keFractions,'LineWidth',options.lineWidth,'Color',options.keFractionColor, DisplayName="KE/(KE+PE)", HandleVisibility='off');
     clabel(C,h,options.keFractions,'Color',options.keFractionColor,'LabelSpacing',options.labelSpacing)
-    % add pseudoWavelength 
-    [C,h] = contour(ax,2*pi./KPseudoLocation/1000,JPseudoLocation,2*pi./kPseudoRadial/1000,options.wavelengths,'LineWidth',options.lineWidth,'Color',options.wavelengthColor, DisplayName="pseudo-wavelength (km)");
+    % add pseudoWavelength. flipud/fliplr gives nicer clabel placement.
+    [C,h] = contour(ax,flipud(2*pi./kPseudoLocation/1000),jPseudoLocation,fliplr(2*pi./kPseudoRadial'/1000),options.wavelengths,'LineWidth',options.lineWidth,'Color',options.wavelengthColor, DisplayName="pseudo-wavelength (km)");
     clabel(C,h,options.wavelengths,'Color',options.wavelengthColor,'LabelSpacing',options.labelSpacing)
     hold off
+    % y axis ticks
+    % select reasonable jMode spacing for ticks
+    jMode = [0:9,10:10:90,100:100:900];
+    jMode = jMode(jMode<=max(self.j)); % truncate at maximum mode
+    jModePseudo = jMode;
+    jModePseudo(1) = jModePseudo(2)/2;
+    % set tick locations
+    set(ax, 'YTick', jModePseudo);
+    set(ax, 'YMinorTick','off')
+    % set tick labels
+    maxLeadDigit = 5;
+    leadDigit = floor(jMode ./ 10.^floor(log10(jMode)));
+    leadDigit(1) = 0;
+    labels = strings(size(jMode));
+    labels(leadDigit <= maxLeadDigit) = string(jMode(leadDigit <= maxLeadDigit));
+    set(ax,'YTickLabel', labels);
 
     % wave energy spectrum
-    % % % ax = nexttile(tl,nColumns + 3);
     ax = nexttile(tl_inner,2);
     val = log10(TE_Apm_j_kR);
-    pcolor(ax,2*pi./kPseudoLocation/1000,2*pi./jPseudoLocation/1000,val), shading flat,
+    pcolor(ax,2*pi./kPseudoLocation/1000,jPseudoLocation,val), shading flat,
     set(gca,'XDir','reverse')
     set(gca,'XScale','log')
-    set(gca,'YDir','reverse')
     set(gca,'YScale','log')
     axis square
     cb = colorbar('southoutside');
     clim(ax,energy_limits);
     title('wave energy spectrum')
-    % title('wave energy spectrum','Units', 'normalized', 'Position', [0.5, 0.93, 0])
     xlabel('horizontal wavelength (km)')
-    % clim(ax,energy_limits);
-    % ylabel("deformation wavelength (km)")
-    % ylabel("vertical mode scale, 2\pi\cdotL_d (km)")
-    ylabel("vertical mode deformation length (km)")
+    ylabel("Vertical mode number")
     set(gca, 'YAxisLocation', 'right','Layer','top','TickLength',[0.015 0.015]);
     colormap(ax, cmLinMono);
     cb.Label.String = "log10(m^3 s^{-2})";
     set(gca,'layer','top'),
     hold on
-    % add frequency contours
-    % omegaPadded = cat(1,self.omega_jk(1,:),self.omega_jk);
-    omegaPadded = cat(1,[self.omega_jk(1,:);self.omega_jk(1,:)],self.omega_jk(1:end-1,:));
-    % omegaPadded = cat(2,omegaPadded(:,1),omegaPadded);
-    omegaPadded = cat(2,[omegaPadded(:,1) omegaPadded(:,1)],omegaPadded(:,1:end-1));
-    omegaJK = interpn(KPaddedWVD,JPaddedWVD,omegaPadded.',KPseudoLocation,JPseudoLocation,"linear");
-    [C,h] = contour(ax,2*pi./KPseudoLocation/1000,2*pi./JPseudoLocation/1000,omegaJK/self.wvt.f,options.frequencies,'LineWidth',options.lineWidth,'Color',options.frequencyColor, DisplayName="frequency (f)", HandleVisibility='off');
+    % add frequency contours. flipud/fliplr gives nicer clabel placement.
+    omegaPadded = cat(1,self.omega_jk(1,:),self.omega_jk);
+    omegaPadded = cat(2,omegaPadded(:,1),omegaPadded);
+    [C,h] = contour(ax,flipud(2*pi./kPseudoLocation/1000),jPseudoLocation,fliplr(omegaPadded(1:end-1,1:end-1)/self.wvt.f),options.frequencies,'LineWidth',options.lineWidth,'Color',options.frequencyColor, DisplayName="frequency (f)", HandleVisibility='off');
     clabel(C,h,options.frequencies,'Color',options.frequencyColor,'LabelSpacing',options.labelSpacing)
-    % add pseudoWavelength 
-    [C,h] = contour(ax,2*pi./KPseudoLocation/1000,2*pi./JPseudoLocation/1000,2*pi./kPseudoRadial/1000,options.wavelengths,'LineWidth',options.lineWidth,'Color',options.wavelengthColor, DisplayName="pseudo-wavelength (km)");
+    % add pseudoWavelength. flipud/fliplr gives nicer clabel placement.
+    [C,h] = contour(ax,flipud(2*pi./kPseudoLocation/1000),jPseudoLocation,fliplr(2*pi./kPseudoRadial'/1000),options.wavelengths,'LineWidth',options.lineWidth,'Color',options.wavelengthColor, DisplayName="pseudo-wavelength (km)");
     clabel(C,h,options.wavelengths,'Color',options.wavelengthColor,'LabelSpacing',options.labelSpacing)
     hold off
-
+        % y axis ticks
+    % select reasonable jMode spacing for ticks
+    jMode = [0:9,10:10:90,100:100:900];
+    jMode = jMode(jMode<=max(self.j)); % truncate at maximum mode
+    jModePseudo = jMode;
+    jModePseudo(1) = jModePseudo(2)/2;
+    % set tick locations
+    set(ax, 'YTick', jModePseudo);
+    set(ax, 'YMinorTick','off')
+    % set tick labels
+    maxLeadDigit = 5;
+    leadDigit = floor(jMode ./ 10.^floor(log10(jMode)));
+    leadDigit(1) = 0;
+    labels = strings(size(jMode));
+    labels(leadDigit <= maxLeadDigit) = string(jMode(leadDigit <= maxLeadDigit));
+    set(ax,'YTickLabel', labels);
 
 end
 
